dd31ca6c8e6d21fe291d11479521119d56670ff3
Devin Simmons <39880645+devinsimmons@users.noreply.github.com>
2018-07-29 20:36:41 -0400

    Update number_of_trips.sql

--- PostGIS_Queries/number_of_trips.sql
+++ PostGIS_Queries/number_of_trips.sql
@@ -1,3 +1,5 @@
+CREATE TABLE all_trip_frequencies
+AS
 SELECT start_station, end_station, member_type, trip_line, count(*) as number_of_trips
 FROM geo_bike_trips
 GROUP BY start_station, end_station, member_type, trip_line
