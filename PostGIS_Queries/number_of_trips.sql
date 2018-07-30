c84d927a631833c298922dd49c3d73b58e625407
Devin Simmons <39880645+devinsimmons@users.noreply.github.com>
2018-07-29 20:20:56 -0400

    Create number_of_trips.sql
    creates a table that shows the number of trips taken for each unique route that occurred in the database. segregated based on membership type

---
+++ PostGIS_Queries/number_of_trips.sql
@@ -0,0 +1,4 @@
+SELECT start_station, end_station, member_type, trip_line, count(*) as number_of_trips
+FROM geo_bike_trips
+GROUP BY start_station, end_station, member_type, trip_line
+ORDER BY number_of_trips desc
