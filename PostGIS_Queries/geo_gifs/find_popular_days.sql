5d27cee41fedf5c928e49ec462156214cd759870
Devin Simmons <39880645+devinsimmons@users.noreply.github.com>
2018-07-31 19:25:33 -0400

    Update find_popular_days.sql

--- PostGIS_Queries/geo_gifs/find_popular_days.sql
+++ PostGIS_Queries/geo_gifs/find_popular_days.sql
@@ -1,3 +1,7 @@
+/* This query determines the number of trips that happened each day for members and casual riders. For my geogifs, 
+I decided to use the most popular day for members (June 12th from my dataset) and the most popular day for casual users
+(June 16th) */
+
 select 
 	member_type,
 	date_trunc('day', start_date) AS date_of_trip, 
