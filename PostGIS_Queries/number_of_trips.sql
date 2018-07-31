579e1b6a0109fd2568d6934a1eea81ff0c0ce375
Devin Simmons <39880645+devinsimmons@users.noreply.github.com>
2018-07-31 09:25:11 -0400

    added description

--- PostGIS_Queries/number_of_trips.sql
+++ PostGIS_Queries/number_of_trips.sql
@@ -1,3 +1,6 @@
+/* This statement queries the main table. It groups the trips based on their origin and destination,
+as well as the member type. It then adds a column showing how many trips are in the group */
+
 CREATE TABLE all_trip_frequencies
 AS
 SELECT start_station, end_station, member_type, trip_line, count(*) as number_of_trips
