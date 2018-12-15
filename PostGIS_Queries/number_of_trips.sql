2c01ecceabc30955ca2cb72e89d1c959f758e31a
Devin Simmons <39880645+devinsimmons@users.noreply.github.com>
2018-12-15 14:38:07 -0500

    removed grouping by member type

--- PostGIS_Queries/number_of_trips.sql
+++ PostGIS_Queries/number_of_trips.sql
@@ -3,7 +3,7 @@ as well as the member type. It then adds a column showing how many trips are in 
 
 CREATE TABLE all_trip_frequencies
 AS
-SELECT start_station, end_station, member_type, trip_line, count(*) as number_of_trips
+SELECT start_station, end_station, trip_line, count(*) as number_of_trips
 FROM geo_bike_trips
-GROUP BY start_station, end_station, member_type, trip_line
+GROUP BY start_station, end_station, trip_line
 ORDER BY number_of_trips desc
